ee08e06bfe40ee0fc9ae671b29aa287744f21a1d
andras-macbook-air <andras@vpmedia.hu>
2022-11-04 12:27:35 +0100

    Migrate to jest for unit testing

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+const config = {
+  verbose: false,
+};
+
+module.exports = config;
--- test/setup.js
+++
@@ -1,5 +0,0 @@
-/**
- * @author       Andras Csizmadia <andras@vpmedia.hu>
- * @copyright    Copyright (c) 2018-present Richard Davey, Photon Storm Ltd., Andras Csizmadia <andras@vpmedia.hu> (www.vpmedia.hu)
- */
-process.env.NODE_ENV = 'test';
