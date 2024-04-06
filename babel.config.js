a7a9daada3658d23eb45de61a765f09392423f05
Andras Csizmadia <andras@vpmedia.hu>
2024-04-06 17:06:31 +0200

    Fixed lint warning

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
-/** @type {import('@babel/core').ConfigFunction} */
+/**
+ * Babel configuration function.
+ * @param {import('@babel/core').ConfigAPI} api - Configuration API reference.
+ * @returns {import('@babel/core').TransformOptions} Configuration options.
+ */
 const config = (api) => {
   api.cache(false);
   const presets = ['@babel/preset-env'];
