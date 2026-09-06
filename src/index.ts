851d431aa60440f4f893c66843097fcc94525485
vpmedia <andras@vpmedia.hu>
2026-09-06 15:12:19 +0200

    feat(types): export the game config and scene manager types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export * as Const from './phaser/core/const.js';
 export { GameObjectFactory } from './phaser/core/factory.js';
 export { Game } from './phaser/core/game.js';
+export type { GameConfig } from './phaser/core/game.js';
 export { Signal } from './phaser/core/signal.js';
 export * as MathUtils from './phaser/util/math.js';
 // cache
@@ -17,6 +18,7 @@ export { ScaleManager } from './phaser/core/scale_manager.js';
 // scene
 export { Scene } from './phaser/core/scene.js';
 export { SceneManager } from './phaser/core/scene_manager.js';
+export type { SceneDefinition, SceneHooks, SceneState } from './phaser/core/scene_manager.js';
 // sound
 export { Sound } from './phaser/core/sound.js';
 export { SoundManager } from './phaser/core/sound_manager.js';
